1ff0b29585b1f9c09410132c6ef90cb4583948da
Babuperumana <babuperumana@gmail.com>
2024-09-15 23:26:44 +0530

    Create events.js

---
+++ events.js
@@ -0,0 +1,6 @@
+// events.js
+function displayUpcomingEvents() {
+    return 'Upcoming Events:\n1. Navaratri - 10 Oct\n2. Diwali - 4 Nov\n...More events...';
+}
+
+module.exports = { displayUpcomingEvents };
